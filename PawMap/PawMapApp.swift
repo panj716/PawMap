29dc5ac09dc6ee9571d3e71716cc7be8bf4787f9
Sunny Pan <sunnypan0716@gmail.com>
2025-09-04 18:37:47 -0400

    Initial Commit

---
+++ PawMap/PawMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawMapApp.swift
+//  PawMap
+//
+//  Created by Sunny on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PawMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PawMapTests/PawMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  PawMapTests.swift
+//  PawMapTests
+//
+//  Created by Sunny on 9/4/25.
+//
+
+import Testing
+@testable import PawMap
+
+struct PawMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
